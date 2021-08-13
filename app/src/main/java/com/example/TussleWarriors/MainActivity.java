9387d7b05ee1a797c72dd545caf177e36f717edc
DivyRaj <divyraj65407@gmail.com>
2021-08-13 15:37:52 +0530

    Final Commit TussleWarriors

--- app/src/main/java/com/example/TussleWarriors/MainActivity.java
+++ app/src/main/java/com/example/TussleWarriors/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.TussleWarriors.databinding.ActivityMainBinding;
@@ -18,6 +21,17 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
     FirebaseAuth auth;
+    private static final int PICK_IMAGE_REQUEST = 234;
+
+    //Buttons
+    private Button buttonChoose;
+    private Button buttonUpload;
+
+    //ImageView
+    private ImageView imageView;
+
+    //a Uri object to store file path
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,4 +67,5 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
